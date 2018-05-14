f6b3f46f0a0cc9da863aeca40f6e09e033d2e7f0
André Secco <andreluizsecco@yahoo.com.br>
2018-05-14 20:54:59 -0300

    Remoção de packages desnecessários

---
+++ EntityTypeConfiguration/Migrations/20180514235329_Initial.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace EntityTypeConfiguration.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Livro",
+                columns: table => new
+                {
+                    LivroId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AnoPublicacao = table.Column<int>(type: "int", nullable: false),
+                    Autor = table.Column<string>(type: "varchar(100)", nullable: true),
+                    DataCadastro = table.Column<DateTime>(type: "date", nullable: false),
+                    Titulo = table.Column<string>(type: "varchar(200)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Livro", x => x.LivroId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LivroDetalhe",
+                columns: table => new
+                {
+                    LivroId = table.Column<int>(type: "int", nullable: false),
+                    Editora = table.Column<string>(type: "varchar(100)", nullable: true),
+                    NumeroPaginas = table.Column<int>(type: "smallint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LivroDetalhe", x => x.LivroId);
+                    table.ForeignKey(
+                        name: "FK_LivroDetalhe_Livro_LivroId",
+                        column: x => x.LivroId,
+                        principalTable: "Livro",
+                        principalColumn: "LivroId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LivroDetalhe");
+
+            migrationBuilder.DropTable(
+                name: "Livro");
+        }
+    }
+}
