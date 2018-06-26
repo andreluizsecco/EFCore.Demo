b533fcfbc8f7fa6eb324a469e21be5be07be9f9e
André Secco <andreluizsecco@yahoo.com.br>
2018-06-25 21:01:19 -0300

    Detalhamento do projeto EntityTypeConfiguration

---
+++ EntityTypeConfiguration/Contexts/DataContext.cs
@@ -0,0 +1,22 @@
+using EntityTypeConfiguration.Entities;
+using EntityTypeConfiguration.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityTypeConfiguration.Contexts
+{
+    public class DataContext : DbContext
+        {
+            public DbSet<Livro> Livros { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFCore.Demo;Trusted_Connection=True;");
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.ApplyConfiguration(new LivroMap());
+                modelBuilder.ApplyConfiguration(new LivroDetalhesMap());
+            }
+        }
+}
--- EntityTypeConfiguration/Program.cs
+++ EntityTypeConfiguration/Program.cs
@@ -1,27 +1,7 @@
-﻿using EntityTypeConfiguration.Entities;
-using EntityTypeConfiguration.Mappings;
-using Microsoft.EntityFrameworkCore;
-
-namespace EntityTypeConfiguration
+﻿namespace EntityTypeConfiguration
 {
     class Program
     {
         static void Main(string[] args) { }
-
-        public class LivrosContext : DbContext
-        {
-            public DbSet<Livro> Livro { get; set; }
-
-            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            {
-                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFCore.Demo;Trusted_Connection=True;");
-            }
-
-            protected override void OnModelCreating(ModelBuilder modelBuilder)
-            {
-                modelBuilder.ApplyConfiguration(new LivroMap());
-                modelBuilder.ApplyConfiguration(new LivroDetalhesMap());
-            }
-        }
     }
 }
